c2b84ad7017118056f855f2875cd79df28f7f48c
Shubham Kumar <shubhamkumarfb90@gmail.com>
2026-05-10 01:42:25 +0530

    third commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,96 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import {
+  component$,
+  useSignal,
+  $,
+  useStylesScoped$,
+} from "@builder.io/qwik";
+
+import styles from "./style.css?inline";
 
 export default component$(() => {
+  useStylesScoped$(styles);
+
+  const questions = [
+    {
+      question: "What does HTML stand for?",
+      options: [
+        "Hyper Text Markup Language",
+        "High Text Machine Language",
+        "Hyper Transfer Markup Language",
+        "Home Tool Markup Language",
+      ],
+      answer: "Hyper Text Markup Language",
+    },
+    {
+      question: "Which language is used for styling web pages?",
+      options: ["HTML", "Python", "CSS", "C++"],
+      answer: "CSS",
+    },
+    {
+      question: "Which is a JavaScript framework?",
+      options: ["React", "Django", "Flask", "Laravel"],
+      answer: "React",
+    },
+  ];
+
+  const currentQuestion = useSignal(0);
+  const score = useSignal(0);
+  const showScore = useSignal(false);
+
+  const checkAnswer = $((option: string) => {
+    if (option === questions[currentQuestion.value].answer) {
+      score.value++;
+    }
+
+    const nextQuestion = currentQuestion.value + 1;
+
+    if (nextQuestion < questions.length) {
+      currentQuestion.value = nextQuestion;
+    } else {
+      showScore.value = true;
+    }
+  });
+
   return (
-    <>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-    </>
+    <div class="quiz-container">
+      <h1>Mini Quiz App</h1>
+
+      {showScore.value ? (
+        <div>
+          <h2>
+            Your Score: {score.value} / {questions.length}
+          </h2>
+
+          <button
+            class="restart-button"
+            onClick$={() => {
+              currentQuestion.value = 0;
+              score.value = 0;
+              showScore.value = false;
+            }}
+          >
+            Restart Quiz
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h2>
+            Question {currentQuestion.value + 1} / {questions.length}
+          </h2>
+
+          <p>{questions[currentQuestion.value].question}</p>
+
+          {questions[currentQuestion.value].options.map((option) => (
+            <button
+              key={option}
+              class="quiz-button"
+              onClick$={() => checkAnswer(option)}
+            >
+              {option}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
-};
